b3c67535a27c1677413cb3d1d2bc50dfe635cf79
unknown <PurviDabhi>
2024-07-25 15:21:29 +0530

    Mongo Connection Added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants"
+
+
+const connectDB = async() =>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    }
+    catch(error){
+        console.log("ERROR : " + error)
+        throw error
+    }
+}
+connectDB()
